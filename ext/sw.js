26fba374910b3da509ef178be213b6fef62a47d6
Jan Kühle <jkuehle90@gmail.com>
2024-03-09 19:03:06 +0100

    Add extension

---
+++ ext/sw.js
@@ -0,0 +1,34 @@
+const appWindows = new Set();
+const appTabs = new Set();
+chrome.windows.onCreated.addListener(
+    (window) => void appWindows.add(window.id),
+    { windowTypes: ["app"] },
+);
+chrome.tabs.onCreated.addListener((tab) => {
+    if (appWindows.has(tab.windowId)) {
+        appTabs.add(tab.id);
+    }
+});
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+    appTabs.delete(tabId);
+    if (removeInfo.isWindowClosing) {
+        appWindows.delete(removeInfo.windowId);
+    }
+});
+chrome.tabs.query({ windowType: "app" }).then((tabs) => {
+    for (const tab of tabs) {
+        appWindows.add(tab.windowId);
+        appTabs.add(tab.id);
+    }
+});
+
+chrome.webNavigation.onCreatedNavigationTarget.addListener((details) => {
+    if (appTabs.has(details.sourceTabId)) {
+        console.log(
+            "External URL opened from app. Opening in Firefox.",
+            details.url,
+        );
+        chrome.tabs.remove(details.tabId);
+        chrome.runtime.sendNativeMessage("native.app.id", { url: details.url });
+    }
+});
