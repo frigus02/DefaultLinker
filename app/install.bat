901d0beea857845cbcf2e9dcc399ed6ea595220b
Jan Kühle <jkuehle90@gmail.com>
2024-03-10 09:45:13 +0100

    Use %cd% in install.bat

--- app/install.bat
+++ app/install.bat
@@ -1,1 +1,1 @@
-REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\me.kuehle.open_app_links_in_default_browser" /ve /t REG_SZ /d "C:\code\open-app-links-in-default-browser\app\manifest.json" /f
+REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\me.kuehle.open_app_links_in_default_browser" /ve /t REG_SZ /d "%cd%\manifest.json" /f
